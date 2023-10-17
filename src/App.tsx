526c9947527d7d7ccea96a4ab8fd23b39029faa1
Michal <michal.erik.czajkowski@gmail.com>
2023-10-17 20:23:55 +0200

    feat: implement card adding - create and style Input component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { AppLayout } from './components/AppLayout';
 import './App.css';
 import Button from './components/UI/Button';
 import Card from './components/UI/Card';
+import Input from './components/UI/Input';
 
 function App() {
   return (
     <AppLayout>
       <AppHeader cardsAmount={0} />
       <Card>
+        <Input />
         <Button>Cancel</Button>
         <Button filled={true}>Next</Button>
       </Card>
