c82e47bda650e7871e2613f65f989a78d33512a5
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-10-29 18:53:42 +0100

    feat: test edditing flashcard - current setup to mock requests. propably not the best idea

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,31 @@
+import "@testing-library/jest-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+
+import App from "../App";
+
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+test("renders App component", async () => {
+  const mockCards = [
+    { id: "1", front: "Front 1", back: "Back 1" },
+    { id: "2", front: "Front 2", back: "Back 2" },
+  ];
+
+  fetchMock.mockResponse(JSON.stringify(mockCards));
+
+  const { debug } = render(<App />);
+  debug();
+
+  // Loading message should be displayed initially
+  expect(screen.getByText("Loading...")).toBeInTheDocument();
+
+  // Wait for the data to load
+  await waitFor(() => {
+    expect(screen.getByText("Front 1")).toBeInTheDocument();
+    expect(screen.getByText("Front 2")).toBeInTheDocument();
+    expect(screen.queryByText("Loading...")).toBeNull();
+  });
+});
