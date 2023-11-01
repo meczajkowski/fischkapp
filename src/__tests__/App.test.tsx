1e32f348a6cb51263d88c49188342f517e799de2
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-02 00:07:52 +0100

    feat: test adding flashcards - remove act from render and fetch

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,12 +1,6 @@
 import "@testing-library/jest-dom";
 import fetchMock from "jest-fetch-mock";
-import {
-  render,
-  screen,
-  act,
-  waitFor,
-  fireEvent,
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../App";
 
 beforeEach(() => {
@@ -17,155 +11,77 @@ beforeEach(() => {
 describe("App", () => {
   // render app with mocked 0 cards
   it("should render app component with new card form closed initially", async () => {
-    await act(async () => {
-      render(<App />);
-      fetchMock.mockResponseOnce(JSON.stringify([]));
-    });
+    fetchMock.mockResponseOnce(JSON.stringify([]));
+    render(<App />);
 
-    await waitFor(() => {
-      expect(screen.queryByText("Loading...")).toBeNull();
-      expect(screen.getByText("Add your first flashcard")).toBeInTheDocument();
-      expect(screen.queryByTestId("new-card-form")).not.toBeInTheDocument();
-      expect(screen.queryByTestId("cards-list")).toBeEmptyDOMElement();
-    });
+    await screen.findByText("Add your first flashcard");
+    expect(screen.queryByTestId("new-card-form")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("cards-list")).toBeEmptyDOMElement();
   });
-
   // open form when CTA clicked
   it("should open the new card form when the 'Add Card' button is clicked.", async () => {
-    await act(async () => {
-      render(<App />);
-      fetchMock.mockResponse(JSON.stringify([]));
-    });
+    fetchMock.mockResponse(JSON.stringify([]));
+    render(<App />);
 
-    const ctaButton = screen.getByRole("button");
+    const ctaButton = await screen.findByRole("button");
     fireEvent.click(ctaButton);
-
     expect(screen.getByTestId("new-card-form")).toBeInTheDocument();
-    // screen.debug();
-
-    // debug returned html after header closing tag ->
-    // why ul is a selfclosing tag?
-
-    //   <div
-    //     data-testid="new-card-form"
-    //   >
-    //     <input
-    //       class=" "
-    //       type="text"
-    //       value=""
-    //     />
-    //     <div>
-    //       <button
-    //         class="undefined  false"
-    //       >
-    //         Cancel
-    //       </button>
-    //       <button
-    //         class="undefined  undefined"
-    //         disabled=""
-    //       >
-    //         Next
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
-    // <ul
-    //   data-testid="cards-list"
-    // />
   });
 
   // dont send form whith front input empty
   it("should not be possible to add a flashcard when front card value is empty", async () => {
-    await act(async () => {
-      render(<App />);
-      fetchMock.mockResponse(JSON.stringify([]));
-    });
+    fetchMock.mockResponse(JSON.stringify([]));
+    render(<App />);
 
-    const ctaButton = screen.getByRole("button");
+    const ctaButton = await screen.findByRole("button");
     fireEvent.click(ctaButton);
 
     expect(screen.getByTestId("new-card-form")).toBeInTheDocument();
 
-    const frontInput = await screen.findByRole("textbox");
+    const frontInput = screen.getByRole("textbox");
     fireEvent.change(frontInput, { target: { value: "" } });
 
     expect(screen.getByText("Next")).toBeDisabled();
-
-    // screen.debug();
-
-    // returns html - button disabled="" ul still selfclosing
-
-    // <div
-    //     class="undefined undefined"
-    //   >
-    //     <div
-    //       data-testid="new-card-form"
-    //     >
-    //       <input
-    //         class=" "
-    //         type="text"
-    //         value=""
-    //       />
-    //       <div>
-    //         <button
-    //           class="undefined  false"
-    //         >
-    //           Cancel
-    //         </button>
-    //         <button
-    //           class="undefined  undefined"
-    //           disabled=""
-    //         >
-    //           Next
-    //         </button>
-    //       </div>
-    //     </div>
   });
 
   // dont send form when back input empty
   it("should not be possible to add a flashcard when back card value is empty", async () => {
-    await act(async () => {
-      render(<App />);
-      fetchMock.mockResponse(JSON.stringify([]));
-    });
+    fetchMock.mockResponse(JSON.stringify([]));
+    render(<App />);
 
-    const ctaButton = screen.getByRole("button");
+    const ctaButton = await screen.findByRole("button");
     fireEvent.click(ctaButton);
 
     expect(screen.getByTestId("new-card-form")).toBeInTheDocument();
 
-    const frontInput = await screen.findByRole("textbox");
+    const frontInput = screen.getByRole("textbox");
     fireEvent.change(frontInput, { target: { value: "This is front" } });
 
-    const nextStepButton = await screen.findByText("Next");
+    const nextStepButton = screen.getByText("Next");
     fireEvent.click(nextStepButton);
 
-    const backInput = await screen.findByRole("textbox");
+    const backInput = screen.getByRole("textbox");
     expect(backInput).toBeInTheDocument();
 
     fireEvent.change(backInput, { target: { value: "" } });
     expect(screen.getByText("Save")).toBeDisabled();
-
-    // screen.debug();
   });
 
   it("should be possible to add a flashcard when front and back card value is not empty", async () => {
-    await act(async () => {
-      render(<App />);
-      fetchMock.mockResponse(
-        JSON.stringify([{ _id: "1", front: "first front", back: "first back" }])
-      );
-    });
+    fetchMock.mockResponse(
+      JSON.stringify([{ _id: "1", front: "first front", back: "first back" }])
+    );
+    render(<App />);
 
-    const ctaButton = screen.getByRole("button");
+    const ctaButton = await screen.findByRole("button");
     fireEvent.click(ctaButton);
 
     expect(screen.getByTestId("new-card-form")).toBeInTheDocument();
 
-    const frontInput = await screen.findByRole("textbox");
+    const frontInput = screen.getByRole("textbox");
     fireEvent.change(frontInput, { target: { value: "This is front" } });
 
-    const nextStepButton = await screen.findByText("Next");
+    const nextStepButton = screen.getByText("Next");
     fireEvent.click(nextStepButton);
 
     const backInput = await screen.findByRole("textbox");
@@ -173,30 +89,26 @@ describe("App", () => {
 
     fireEvent.change(backInput, { target: { value: "This is back" } });
 
-    await act(async () => {
-      // Mock the POST request to add the new card.
-      fetchMock.mockResponse(
-        JSON.stringify([
-          { _id: "2", front: "This is front", back: "This is back" },
-        ])
-      );
+    // Mock the POST request to add the new card.
+    fetchMock.mockResponse(
+      JSON.stringify([
+        { _id: "2", front: "This is front", back: "This is back" },
+      ])
+    );
 
-      fireEvent.click(screen.getByText("Save"));
+    fireEvent.click(screen.getByText("Save"));
 
-      // Mock the GET request again to fetch the updated list of cards.
-      fetchMock.mockResponse(
-        JSON.stringify([
-          { _id: "1", front: "first front", back: "first back" },
-          { _id: "2", front: "This is front", back: "This is back" },
-        ])
-      );
-    });
+    // Mock the GET request again to fetch the updated list of cards.
+    fetchMock.mockResponse(
+      JSON.stringify([
+        { _id: "1", front: "first front", back: "first back" },
+        { _id: "2", front: "This is front", back: "This is back" },
+      ])
+    );
 
-    const initialCard = screen.getByText("first front");
-    const addedCard = screen.getByText("This is front");
+    const initialCard = await screen.findByText("first front");
+    const addedCard = await screen.findByText("This is front");
     expect(initialCard).toBeInTheDocument();
     expect(addedCard).toBeInTheDocument();
-
-    screen.debug();
   });
 });
