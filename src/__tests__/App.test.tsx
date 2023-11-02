21a8f1e7e8ec69a199656af7a47b9d4e6e832718
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-02 13:31:59 +0100

    feat: test editing flashcard - save card when input is not empty

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -194,13 +194,48 @@ describe('Editing flashcard', () => {
     const saveButton = screen.getByText('Save');
     expect(saveButton).toBeDisabled();
   });
-  // change input value
-  // mock patch req with chenged value
-  //  find save btn and save card
-  // mock get req
-  // expect card with new value
-});
 
-// It should not be possible to edit a flashcard by clicking Save button when edited value is empty
-// It should be possible to edit a flashcard by clicking Save button when edited value is not empty
-// It should be possible to exit editing mode by clicking cancel button
+  // save card when input is not empty
+  it('should be possible to edit a flashcard by clicking Save button when edited value is not empty', async () => {
+    fetchMock.mockResponse(
+      JSON.stringify([
+        { _id: '1', front: 'initial front', back: 'initial back' },
+      ])
+    );
+    render(<App />);
+
+    expect(await screen.findByTestId('cards-list')).not.toBeEmptyDOMElement();
+    expect(screen.getByText('initial front'));
+
+    const editButton = screen.getByTestId('edit-icon');
+    fireEvent.click(editButton);
+
+    expect(screen.getByTestId('edit-form'));
+
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveValue('initial front');
+
+    fireEvent.change(input, { target: { value: 'edited front' } });
+    const saveButton = screen.getByText('Save');
+    expect(saveButton).not.toBeDisabled();
+
+    // mock patch req with changed value
+    fetchMock.mockResponse(
+      JSON.stringify([
+        { _id: '1', front: 'edited front', back: 'initial back' },
+      ])
+    );
+
+    fireEvent.click(saveButton);
+
+    // Mock the GET request again to fetch the updated list of cards.
+    fetchMock.mockResponse(
+      JSON.stringify([
+        { _id: '1', front: 'edited front', back: 'initial back' },
+      ])
+    );
+
+    const editedCard = await screen.findByText('edited front');
+    expect(editedCard).toBeInTheDocument();
+  });
+});
