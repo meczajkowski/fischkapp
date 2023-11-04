8962df9e37a6abb310eac2f8de2b5fb8d59f8990
meczajkowski <michal.erik.czajkowski@gmail.com>
2023-11-04 02:25:11 +0100

    change: auto resize textarea

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './Input.module.css';
 
 interface InputProps {
@@ -9,9 +9,20 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = (props) => {
   const classNames = [styles.input, props.className].join(' ');
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto'; // Reset the height to auto
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + 'px'; // Set the height based on the content
+    }
+  }, [props.value]);
 
   return (
     <textarea
+      rows={1}
+      ref={textareaRef}
       value={props.value}
       onChange={props.onChange}
       className={classNames}
